39d5400d0d8acd6cad953e47770ab919b350f2dc
njaltare <nikhiljaltare87@gmail.com>
2022-02-19 20:10:22 +0530

    first commit

---
+++ com.mashery.webapps/src/test/java/com/mashery/webapps/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mashery.webapps;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void welcomeTest() {
+		System.out.println("Hi Selenium");
+	}
+}
